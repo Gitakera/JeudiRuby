a285867734c3d3068f13c5a6a3f65c73694efb3c
Rasit1 <rasitramiakatra@gmail.com>
2019-01-10 16:41:43 +0300

    stairway semi final

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -4,28 +4,28 @@ puts"GAME GAME GAME"
 @compteurLancer=0
 
 def resultat(resultD,position)
-    
+
     if position!=10
-      
+
       if resultD==5 || resultD==6
         @etageActuel=@etageActuel+1
-        #puts"Vous avancez d'un etage, ce qui vous positionne à l'étage : #{@etageActuel}"
-         
+        puts"Montée d'une étage : #{@etageActuel}"
+
       end
-      
+
       if resultD==1
         @etageActuel=@etageActuel-1
-        #puts"Vous descendez d'un étage, ce qui vous positionne à l'étage : #{@etageActuel}"
-        
+        puts"Descente d'une étage : #{@etageActuel}"
+
       end
-      
+
       if resultD==2 || resultD==3 || resultD==4
-        #puts" Ups ! vous restez là ou vous étiez, à l'étages : #{@etageActuel}"
-        
+        puts"Stagne sur l'étage : #{@etageActuel}"
+
       end
-              
+
     end
-      
+
 end
 
 def saisieD
@@ -35,13 +35,13 @@ def saisieD
   d=Random.rand(1..6)
   @compteurLancer=@compteurLancer+1
   d=d.to_i
-  #puts"On a #{d}"
-  
+  puts"On a #{d}"
+
   return d
-end 
+end
 
 def traceur(position=@etageActuel)
-  
+
   for i in 1..10
     if i==11-position
       puts"|<¤>"
@@ -49,32 +49,32 @@ def traceur(position=@etageActuel)
       puts"|--"
     end
   end
-  
+
 end
 
 
-puts"Tapez entrer pour demarrer la simulation"
+puts"Tapez ENTREE pour démarrer la simulation"
 n=gets.chomp
 def average_finish_time
 
 tr=0
 while tr<100
-   @etageActuel=0   
+   @etageActuel=0
     while @etageActuel!=10
-    
+
       resultat( saisieD,@etageActuel)
       #pour reperer graphiquement la position
       #traceur(@etageActuel)
-      
-        if @etageActuel<0 
+
+        if @etageActuel<0
           @etageActuel=0
         end
-        
+
         if @etageActuel==10
-               #puts"SUPER TU EST ARRIVE AU PLUS HAUT NIVEAU. BRAVO !" 
-                 
+               puts "NIVEAU MAX ATTEINT. BRAVO!!"
+
         end
-        
+
     end
 tr=tr+1
 end
