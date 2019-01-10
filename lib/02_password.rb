ace0ff8106152de872e69f707ede37f1063fba2e
Rasit1 <rasitramiakatra@gmail.com>
2019-01-10 15:51:04 +0300

    password edited

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,5 +1,5 @@
 
-@pwd="" 
+@pwd=""
 def signup
   puts"WELCOME TO Ramsès community , please give a new password :"
   print"->"
@@ -7,32 +7,38 @@ def signup
 end
 
 def login
-  
+
   pwdcheck=""
+  i=0
   while @pwd!=pwdcheck
-    puts"huhuu ! now enter your password and you'll be welcome"
+    puts"Now enter your password and you'll be welcomed"
     print"Password : >"
     pwdcheck=gets.chomp
+if i>0 || @pwd!=pwdcheck
+  puts "Wrong password! try again"
   end
-  
+end
+
+
+
   if @pwd=""
-    puts"Vous avez saisie une mots de passe vide donc on passe direct-to->"
+    puts"Vous avez saisi un mot de passe vide donc on passe direct-to->"
   end
-  
+
 end
 
 
 def welcome_screen
   puts"=============================WELCOM TO YOU, THIS IS THE HACKING PROJECT =================================== "
-  puts" ###        ##          ###      ##########    ##        ############         ##########      ###                ###"             
-  puts"  ##      ##  ##       ##        ##            ##        ##                   ##      ##      ##  ##          ##  ##"
-  puts"   ##    ##    ##     ##         ##########    ##        ##                   ##      ##      ##    ##       ##   ##"
-  puts"    ##  ##      ##   ##          ##            ##        ##                   ##      ##      ##     ##    ##     ##"
-  puts"      ##          ##             ##########    ########  ############         ##########      ##       ###        ##"
+  puts" ###        ##          ###    ##########    ##        ############   ##########    ###                ###"
+  puts"  ##      ##  ##       ##      ##            ##        ##             ##      ##    ##  ##          ##  ##"
+  puts"   ##    ##    ##     ##       ##########    ##        ##             ##      ##    ##    ##       ##   ##"
+  puts"    ##  ##      ##   ##        ##            ##        ##             ##      ##    ##     ##    ##     ##"
+  puts"      ##          ##           ##########    ########  ############   ##########    ##       ###        ##"
   puts"------------------------------------------------------------------------------------------------------------"
   puts""
   puts""
-  puts"SECRET: Ci-dessus est le message trouver dans le mobile de Amir de SAYNA :p ---> IL EST PAS SI FORT EN ANGLAIS :P "
+  puts"SECRET: Ci-dessus est le message trouvé dans le mobile de Amir de SAYNA :p ---> IL EST PAS SI FORT EN ANGLAIS :P "
   puts""
   puts""
   puts""
@@ -40,8 +46,8 @@ def welcome_screen
   puts"PS: CACHE BIEN TON SCREEN CAR IL PEUT TE VOIR"
   puts""
   puts""
-  puts" C'est tout, ton mots de pass est #{@pwd} :p haha ! tu t'est fait hacké . bye! Merci ;)"
-  
+  puts"C'est tout, ton mot de passe est #{@pwd} :p haha ! tu t'est fait hacké . bye! Merci ;)"
+
 
 end
 
